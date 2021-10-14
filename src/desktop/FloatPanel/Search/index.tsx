d9217c1c102e278c7301a8a39c332b2764f075f8
German Mitish <german.mitish@icloud.com>
2021-10-14 16:22:59 -0400

    Added state to search component

--- src/desktop/FloatPanel/Search/index.tsx
+++ src/desktop/FloatPanel/Search/index.tsx
@@ -2,7 +2,7 @@ import { useI18n } from "shared";
 import { SearchInput, SearchWrapper, FieldWrapper, SearchResults } from "../styles";
 import { ReactComponent as SearchIcon } from 'assets/icons/search.svg'
 import { ReactComponent as CancelIcon } from 'assets/icons/cancel.svg'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Buttons from "./Buttons";
 import { useSearchResultsSubscription } from 'generated'
 import { useRecoilState } from "recoil";
@@ -12,10 +12,18 @@ export default function Search(){
   const i18n = useI18n()
   const [keyword, setKeyword] = useState('')
   const [layers, setLayers] = useRecoilState(Map.layers)
-
   const {data} = useSearchResultsSubscription({variables:{layers,keyword: `%${keyword}%`}})
+  const [searchResults, setSearchResults] = useState(data)
 
-  console.log(data)
+  useEffect(()=>{
+    if(data?.search_entries && data.search_entries.length>0){
+      setSearchResults(data)
+    }
+    if(keyword===''){
+      setSearchResults(undefined)
+    }
+  },[data, setSearchResults, keyword])
+  
   return <>
     <SearchWrapper>
       <Buttons/>
@@ -29,7 +37,7 @@ export default function Search(){
         <div><button onClick={()=>setKeyword('')}><CancelIcon/></button></div>
       </FieldWrapper>
       <SearchResults data-active={!!keyword}>
-        {data?.search_entries.map((v,key)=>
+        {searchResults?.search_entries.map((v,key)=>
           <ListRow onClick={()=>setKeyword('')} data={v as any} {...{key}} />
         )}
       </SearchResults>

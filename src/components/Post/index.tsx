295fb820b19646d0acb693a74a900cf6b3e9a087
Ivan Milaev <ivan.milaev@dreamsoft3d.com>
2021-10-27 22:41:00 +0300

    post component  progress

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,13 +1,22 @@
 import { IPost } from "./IPost";
-import { Container } from "./styles";
+import { Author, Avatar, Container, Message, Name, Roles } from "./styles";
+import UserIcon from 'assets/icons/user.svg'
 
 export interface IPostProps extends IPost {}
 
 function Post({author,  message}:IPostProps) {
     return(
         <Container>
-            {author}
-            {message}
+            <Author>
+                <Avatar>
+                    <img src={UserIcon} alt="user"/>
+                </Avatar>
+                <Name></Name>
+                <Roles></Roles>
+            </Author>
+            <Message>
+            Всім привіт! Я майстер і готовий встановити або зробити лаву.
+            </Message>
         </Container>
     )
 }
--- src/components/Post/styles.tsx
+++ src/components/Post/styles.tsx
@@ -1,6 +1,32 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
+    width: 100%;    
     display: flex;
     flex-direction: column;
+    background-color: ${props =>  props.theme.colors.backgroundLighter};
+    box-shadow: 0px 0px 2px rgba(137, 150, 159, 0.1), 0px 2px 2px rgba(137, 150, 159, 0.1), 0px 1px 3px rgba(137, 150, 159, 0.1);
+    border-radius: 3px;
+    margin-bottom: 6px;
+    padding: 15px 20px;
     `
+
+export const Author  = styled.div`
+`
+export const Avatar = styled.div``
+
+export const Name = styled.div``
+
+export const Roles = styled.div``
+
+export const Date = styled.div``
+
+export const Message = styled.div`
+    ${props =>  props.theme.font.body.regular.t4};
+    padding: 15px 0;
+`
+
+export const Tags = styled.div``
+
+export const Actions = styled.div``
+    

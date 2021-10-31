c6e7136568205a935c6790bdf4a66a499bec147c
Ivan Milaev <ivan.milaev@dreamsoft3d.com>
2021-11-01 01:10:16 +0200

    implement section tab component

---
+++ src/components/SectionTab/ISectionTab.ts
@@ -0,0 +1,4 @@
+export interface ISectionTab {
+    label: string;
+    disabled?: boolean;
+}
---
+++ src/components/SectionTab/styles.tsx
@@ -0,0 +1,40 @@
+import styled, {css} from 'styled-components'
+import { ISectionTabProps } from '.'
+
+const disabled  =  css`
+    background-color: ${props => props.theme.colors.backgroundLight};
+    opacity: 0.4;
+`
+
+
+export const Container = styled.div.attrs((props: ISectionTabProps) => ({
+}))<ISectionTabProps>`
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    
+    ${props => props.theme.font.body.regular.t5};
+    color: ${props => props.theme.colors.label};
+    border: none;
+    border-radius: 3px;
+    background-color: white;
+    padding: 4px 7px;
+    margin-right: 10px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+
+    &:hover {
+        background-color: ${props => props.theme.colors.backgroundActive};
+    }
+
+    &:active {
+        background-color: ${props => props.theme.colors.backgroundLight};
+    };
+    &:disabled {
+        background-color: ${props => props.theme.colors.backgroundLight};
+        opacity: 0.4;
+    }
+
+    ${props => props.disabled ? disabled: ''}
+`

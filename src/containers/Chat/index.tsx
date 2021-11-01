4748838bb5cfcd1c57498fedc9345285b0538468
Ivan Milaev <ivan.milaev@dreamsoft3d.com>
2021-11-01 21:40:08 +0200

    start implement  initiative chat

---
+++ src/containers/Chat/index.tsx
@@ -0,0 +1,51 @@
+
+import { IChat } from "./IChat";
+import { ChatMessages, Container, CreateMessage, Initiative, Manager, ManagerHeader, Messages, MessagesHeader, Search, Title, UserCard, Users } from "./styles";
+
+export interface IChatProps extends IChat {
+
+}
+
+
+function Chat({}: IChatProps) {
+    return (
+        <Container>
+            <Manager>
+                <ManagerHeader>
+                    <Title>
+                    Title
+                    </Title>
+                    <Initiative>
+                    Initiative
+                    </Initiative>
+                </ManagerHeader>
+                <Search>
+                Search
+                </Search>
+                <Users>
+                Users
+                </Users>
+
+            </Manager>
+            <Messages>
+                <MessagesHeader>
+                    <UserCard>
+                    UserCard
+                    </UserCard>
+                </MessagesHeader>
+
+                <ChatMessages>
+                ChatMessages
+                </ChatMessages>
+
+                <CreateMessage>
+                CreateMessage
+                </CreateMessage>
+
+
+            </Messages>
+        </Container>
+    )
+}
+
+export default Chat;
---
+++ src/containers/Chat/styles.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const  Container = styled.div``,
+
+Manager = styled.div``,
+
+ManagerHeader = styled.div``,
+
+Title = styled.div``,
+
+Initiative = styled.div``,
+
+Search = styled.div``,
+
+Users = styled.div``,
+
+Messages = styled.div``,
+
+MessagesHeader = styled.div``,
+
+UserCard = styled.div``,
+
+ChatMessages = styled.div``,
+
+CreateMessage = styled.div``
+
+
+

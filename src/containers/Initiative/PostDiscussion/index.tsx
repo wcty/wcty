a3ae10b9585d2b2aa2d372652b70cea581e9a714
Ivan Milaev <ivan.milaev@dreamsoft3d.com>
2021-11-01 19:11:46 +0200

    post discussion  progress

--- src/containers/Initiative/PostDiscussion/index.tsx
+++ src/containers/Initiative/PostDiscussion/index.tsx
@@ -1,8 +1,15 @@
 
+import  UserPost from "components/Post";
 import { IPostDiscussion } from "./IPostDiscussion";
-import { Comments, Container, Expander, Header, Post } from "./styles";
+import { Channels, Comments, Container, Expander, Header, Post, Title } from "./styles";
 
-
+const post = {
+    author: {name: "Анатолій Бойко", date : new Date(), roles: ['спеціаліст', 'волонтер', 'інвестор']},
+    message: "Всім привіт! Я майстер і готовий встановити або зробити лаву.",
+    tags: ['розроблення-проєкту'],
+    comments: [{author: "User1", message: "cool", date: new Date()}],
+    likes: [{user:"User 1", date: '5 серпня 2020'}] 
+  }
 export interface IPostDiscussionProps extends IPostDiscussion {}
 
 
@@ -10,11 +17,16 @@ function PostDiscussion({}:IPostDiscussionProps) {
     return (
         <Container>
             <Header>
-                Header
+                <Title>
+                    Лавочка біля будинку
+                </Title>
+                <Channels>
+                    #збір-коштів #розробка-проєкту
+                </Channels>
             </Header>
 
             <Post>
-                Post
+                <UserPost {...post} />
             </Post>
 
             <Expander>
--- src/containers/Initiative/PostDiscussion/styles.tsx
+++ src/containers/Initiative/PostDiscussion/styles.tsx
@@ -1,8 +1,25 @@
 import styled from "styled-components"
 
-export const  Container = styled.div``,
+export const  Container = styled.div`
+    width: 100%;    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`,
 
 Header = styled.div``,
+
+Title = styled.div`
+    ${props => props.theme.font.title.mono.h2}
+    text-align: center;
+`,
+
+Channels = styled.div`
+    ${props => props.theme.font.body.regular.t5}
+    text-align: center;
+`,
+
 
 Post = styled.div``,
 

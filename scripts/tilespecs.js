c55fe9cc91109e64c95a322cc57e45631c88e756
German Mitish <german.mitish@icloud.com>
2021-11-03 16:19:25 -0400

    Try to fix tilespecs

--- scripts/tilespecs.js
+++ scripts/tilespecs.js
@@ -3,4 +3,17 @@ const fs =require('fs')
 
 fetch("https://tiles.weee.city/index.json")
   .then(d=>d.json())
-  .then(d=>fs.writeFile('src/generated/tilespecs.json', JSON.stringify(d, null, 2), { flag: 'w' }, err => {}))
+  .then(d=>{
+    const data = Object.entries(d)
+      .sort(function(a, b) {
+        var nameA = a[0].toUpperCase(); // ignore upper and lowercase
+        var nameB = b[0].toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;}
+        if (nameA > nameB) {
+          return 1;}
+        return 0;
+      })
+      .reduce((agg,v)=>({...agg, [v[0]]:v[1]}), {})
+    fs.writeFile('src/generated/tilespecs.json', JSON.stringify(data, null, 2), { flag: 'w' }, err => {})
+  })

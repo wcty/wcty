dbc1845f27503ae2989668cc3d0027f052a1ca25
German Mitish <german.mitish@icloud.com>
2021-11-03 16:26:52 -0400

    Fixed tilespecs file issue

--- scripts/tilespecs.js
+++ scripts/tilespecs.js
@@ -1,19 +1,28 @@
 global.fetch = require('node-fetch');
 const fs =require('fs')
 
+function Order(val){
+  return Object.entries(val)
+  .sort(function(a, b) {
+    var nameA = a[0].toUpperCase(); // ignore upper and lowercase
+    var nameB = b[0].toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;}
+    if (nameA > nameB) {
+      return 1;}
+    return 0;
+  })
+  .reduce((agg,v)=>({...agg, [v[0]]:v[1]}), {})
+}
+
 fetch("https://tiles.weee.city/index.json")
   .then(d=>d.json())
   .then(d=>{
-    const data = Object.entries(d)
-      .sort(function(a, b) {
-        var nameA = a[0].toUpperCase(); // ignore upper and lowercase
-        var nameB = b[0].toUpperCase(); // ignore upper and lowercase
-        if (nameA < nameB) {
-          return -1;}
-        if (nameA > nameB) {
-          return 1;}
-        return 0;
+    let data = Order(d)
+    data = Object.entries(data)
+      .map(entry=>{
+        return [entry[0], {...entry[1], properties:Order(entry[1].properties)}]
       })
-      .reduce((agg,v)=>({...agg, [v[0]]:v[1]}), {})
+
     fs.writeFile('src/generated/tilespecs.json', JSON.stringify(data, null, 2), { flag: 'w' }, err => {})
   })

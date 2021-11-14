3706e82dbca05e6eb4bfebc9378834be4880e8e6
German Mitish <german.mitish@icloud.com>
2021-11-14 14:46:32 -0500

    Removed index.html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -27,15 +27,27 @@ export default class MyDocument extends Document {
       sheet.seal()
     }
   }
-  // render(): JSX.Element {
-  //   return (
-  //     <Html>
-  //       <Head />
-  //       <body>
-  //         <Main />
-  //         <NextScript />
-  //       </body>
-  //     </Html>
-  //   )
-  // }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta
+            name="description"
+            content="The web platform to help civic self-organisation happen in the cities"
+          />
+          <link rel="apple-touch-icon" href="logo192.png" />
+          <link rel="manifest" href="manifest.json" />
+          <title>We.City</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }

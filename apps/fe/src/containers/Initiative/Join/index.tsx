a8bd668a76bcf33763b6fc67c63a1b2d3b464b3d
Herman Mitish <german.mitish@icloud.com>
2023-04-17 14:28:14 +0200

    fix: join texts

--- apps/fe/src/containers/Initiative/Join/index.tsx
+++ apps/fe/src/containers/Initiative/Join/index.tsx
@@ -1,4 +1,4 @@
-import {  useUser } from 'common';
+import { useUser } from 'common';
 import { Button, Checkbox, TextArea, TextField } from "@ui";
 import { InitiativeByPkDocument, Roles_Enum, useJoinMutation, useTasksQuery } from 'generated';
 import { useEffect, useState } from 'react';
@@ -10,8 +10,8 @@ import { position, layout } from 'styled-system'
 export default function Join() {
   const { id } = useRouter().query;
   const user = useUser()
-  const { data:tasks, error } = useTasksQuery({variables:{id}, fetchPolicy:'cache-first', nextFetchPolicy: 'cache-only' })
-  
+  const { data: tasks, error } = useTasksQuery({ variables: { id }, fetchPolicy: 'cache-first', nextFetchPolicy: 'cache-only' })
+
   const [commitments, setCommitments] = useState({
     donation: {
       value: '',
@@ -39,137 +39,116 @@ export default function Join() {
     variables: {
       id,
       userId: user?.id,
-      donations: 
+      donations:
         (
-          donationChecked && 
+          donationChecked &&
           !isNaN(Number(donationValue)) &&
           Number(donationValue) > 0
-        )? [{
+        ) ? [{
           currency: currency,
           amount: Number(donationValue)
-        }]:[],
+        }] : [],
       tasks:
         (
           taskChecked &&
           taskDescription.length > 0
-        )? [{
+        ) ? [{
           description: taskDescription,
-          volunteers: {data:[{
-            role: Roles_Enum.Initiator,
-            user_id: user?.id
-          }]}
-        }]:[],
+          volunteers: {
+            data: [{
+              role: Roles_Enum.Initiator,
+              user_id: user?.id
+            }]
+          }
+        }] : [],
       volunteers:
         (
           taskChecked &&
-          taskIds.length>0
-        )? taskIds.map(v=>({
+          taskIds.length > 0
+        ) ? taskIds.map(v => ({
           task_id: v,
           role: Roles_Enum.Volunteer,
-        })):[],
-      message: donationChecked && taskChecked? (t`I am ready to donate and volunteer` + (taskDescription.length>0? `: ${taskDescription}` : ''))
-      : donationChecked? t`I am ready to donate` 
-      : taskChecked? (t`I am ready to volunteer` + + (taskDescription.length>0? `: ${taskDescription}` : '')) : ''
+        })) : [],
+      message: donationChecked && taskChecked ? (t`I am ready to donate and volunteer` + (taskDescription.length > 0 ? `: ${taskDescription}` : ''))
+        : donationChecked ? t`I am ready to donate`
+          : taskChecked ? (t`I am ready to volunteer` + + (taskDescription.length > 0 ? `: ${taskDescription}` : '')) : ''
     },
     refetchQueries: [InitiativeByPkDocument]
   });
 
-  const disabled =  !(
+  const disabled = !(
+    (
+      donationChecked &&
+      !isNaN(Number(donationValue)) &&
+      Number(donationValue) > 0
+    ) ||
+    (
+      taskChecked &&
       (
-        donationChecked && 
-        !isNaN(Number(donationValue)) && 
-        Number(donationValue) > 0
-      ) || 
-      (
-        taskChecked &&
-        (
-          taskDescription.length >= 10 ||
-          taskIds.length>0
-        )
-      ) || 
-      (
-        projectChecked &&
-        projectId
+        taskDescription.length >= 10 ||
+        taskIds.length > 0
       )
+    ) ||
+    (
+      projectChecked &&
+      projectId
     )
-  
+  )
+
   return (
     <Container>
       <Form>
         <h3><Trans>Join the initiative</Trans></h3>
         <div>
-          <Checkbox 
-            checked={commitments.donation.checked} 
+          <Checkbox
+            checked={commitments.donation.checked}
             value='donation'
-            onChange={()=>setCommitments({
-              ...commitments, 
-              donation:{
+            onChange={() => setCommitments({
+              ...commitments,
+              donation: {
                 ...commitments.donation,
                 checked: !commitments.donation.checked,
                 value: '1'
               }
-            })}/>
+            })} />
           <Trans>I am ready to donate</Trans>
         </div>
-        
+
         <div>
-          <Checkbox 
-              checked={commitments.task.checked}
-              value='task'
-              onChange={()=>setCommitments({
-                ...commitments, 
-                task:{
-                  ...commitments.task,
-                  checked: !commitments.task.checked
-                }
-              })}/>
-            <Trans>I am ready to volunteer</Trans>
+          <Checkbox
+            checked={commitments.task.checked}
+            value='task'
+            onChange={() => setCommitments({
+              ...commitments,
+              task: {
+                ...commitments.task,
+                checked: !commitments.task.checked
+              }
+            })} />
+          <Trans>I am ready to volunteer</Trans>
         </div>
-        {tasks?.initiative_tasks && 
-          <TaskSelection>
-            <span><Trans>Choose one of the tasks you can perform, or suggest your own.</Trans></span>
-            {tasks?.initiative_tasks.filter(t=>t.description!=='Create initiative').map((t,key)=>
-              <span key={key}>
-                <Checkbox 
-                  checked={commitments.task.ids.includes(t.id)}
-                  value={t.id}
-                  onChange={()=>setCommitments({
-                    ...commitments, 
-                    task:{
-                      ...commitments.task,
-                      checked: (!commitments.task.ids.includes(t.id)) ||
-                        commitments.task.description.length>0,
-                      ids: commitments.task.ids.includes(t.id)?
-                        commitments.task.ids.filter(v=>v!==t.id):
-                        commitments.task.ids.concat(t.id)
-                    }
-                  })}/>
-                {t.description}
-              </span>
-            )}
-          </TaskSelection>}
         <TextArea
           cols={40} rows={3}
           withCancel
-          value={commitments.task.description||''}
-          onChange={(e)=>setCommitments({
-            ...commitments, 
-            task:{
+          value={commitments.task.description || ''}
+          onChange={(e) => setCommitments({
+            ...commitments,
+            task: {
               ...commitments.task,
-              checked: e.target.value!=='' || commitments.task.ids.length>0,
+              checked: e.target.value !== '' || commitments.task.ids.length > 0,
               description: e.target.value
             }
           })}
-          placeholder={t`Suggest a relevant task for the initiative that you can perform`}/>
+          placeholder={t`Suggest a relevant task for the initiative that you can perform`} />
         <div>
           <Button
             s='medium'
             t='primary'
-            disabled={ disabled }
-            onClick={(e)=>{
-              if(user){ 
+            disabled={disabled}
+            onClick={(e) => {
+              if (user) {
                 e.preventDefault();
-                join() 
+                join()
               }
             }}><Trans>Join</Trans></Button>
         </div>
@@ -179,4 +158,4 @@ export default function Join() {
   );
 }
 
-export {default as LoginToJoin} from './LoginToJoin';
+export { default as LoginToJoin } from './LoginToJoin';
